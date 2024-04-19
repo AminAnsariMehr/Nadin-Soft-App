9f686d0d6d61bcf8fa165d774990f58b954a49f1
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-04-19 05:09:31 +0330

    feat: coding logic user data and save in localStorage

--- assets/scripts/pages/loginPage.js
+++ assets/scripts/pages/loginPage.js
@@ -10,3 +10,21 @@ welcomeGoBtn.addEventListener("click", () => {
     welcomeSlide.style.transform = "translate(100%)";
     userDataForm.style.transform = "translate(100%)";
 })
+
+// submit user data Login
+userDataForm.addEventListener("submit", e => {
+    if (usernameInput.value.trim()) {
+        userData.username = usernameInput.value;
+        radioButtons.forEach(element => {
+            if (element.checked) {
+                userData.gender = element.value;
+            }
+        });
+        localStorage.setItem("userData", JSON.stringify(userData));
+        localStorage.setItem("isLogin", 1)
+        location.replace("index.html");
+    } else {
+        alert("Please fill all fields!")
+    }
+    e.preventDefault();
+});
